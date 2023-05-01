80312d7ce810b8ba5db477dc295d4b53a1b55cc9
SeungkyunNA <n.jake270705@gmail.com>
2023-05-01 15:58:47 +0200

    b3

--- Blatt03/AufgabeB3A1.java
+++ Blatt03/AufgabeB3A1.java
@@ -1,49 +1,226 @@
-package Blatt03;
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+
 
 public class AufgabeB3A1{
     public static void main(String[] args) {
+        int[] result;
+        try{
+            result = readStandardIn();
+        } catch (NumberFormatException e){  // Fehlerbehandlung : wenn ungültige Eingabe gegeben wird. (z.B. "a" , " " ...)
+            System.err.println("ERROR: Input is not valid."); 
+            return;
+        }
+        
+        int k;
+        if (args.length == 0) { // Fehlerbehandlung : wenn k nicht eingegeben wird.
+            System.err.println("ERROR: The argument for subsetsize must be one natural number.");
+            return;
+        }
 
-        int[] result = readStandardIn();
-        System.out.println(Arrays.toString(result));
+        try {
+            k = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) { // Fehlerbehandlung : wenn k nicht eine Natürliche Zahl ist. ("1,1" , "a")
+            System.err.println("ERROR: The argument for subsetsize must be one natural number.");
+            return;
+        }
+
+        if (k < 0) {    // Fehlerbehandlung : wenn k nicht eine negative Zahl ist.
+            System.err.println("ERROR: The argument for subsetsize must be one natural number.");
+            return;
+        }
+
+        if(k == 0) {    // Early Return : Es gibt immmer ein leere Array mit 1 Möglichkeit wenn k = 0 ist.
+            int [] err = new int [0];
+            System.out.println(err);
+            System.out.println("There are 1 subsets");
+            return;
+        }
+
+        AufgabeB3A1 a = new AufgabeB3A1(k);
+        int[] remove = a.removeDuplicates(result); // Duplikation abnehmen
+        
+
+        if(remove.length < k) { // Fehlerbehandlung : Falls n < k ist  
+            System.err.println("ERROR: k is too large.");
+            return;
+        }
+
+        int[][] subSets = a.createSubsets(remove);
+        if(subSets == null) {   // Fehlerbehandlung : Wenn Eingabemenge nicht eingegeben wird.  
+            System.err.println("ERROR: Set not entered.");
+            return;
+        }
+
+
+        for(int[] c : subSets) {    // Ergebnis ausgeben
+            System.out.println(Arrays.toString(c));
+        }
+        System.out.println("There are " + subSets.length + " subsets");
 
     }
     public static int[] readStandardIn() throws NumberFormatException {
 
-        Scanner sc = new Scanner(System.in);
-        List<Integer> ls = new ArrayList<Integer>();
+        /* Prinzipiel ist aus Übung. (dynamische Eingabespeicherung mit statische Datenstructur.) */
 
-        while (sc.hasNextLine()) {
-            try {
-                int integerValue = Integer.parseInt(sc.nextLine());
-                ls.add(integerValue);
-            } catch (NumberFormatException e) {
-                System.err.println("Ungueltige Eingabe");
-                sc.close();
+        Scanner sc = new Scanner(System.in);
+        
+        int[] a = new int[1];
+        int i = 0;
+        while(sc.hasNextLine()) {
+            
+            int target = Integer.parseInt(sc.nextLine());   // Fehlerbehandlung unnötig. Es wird in Main() abgefangen.
+            if (i < a.length) {
+                a[i] = target;
+                i++;
+            } else {
+                int[] aa = new int[a.length*2];
+                for (int p=0;p<a.length;p++){
+                    aa[p]=a[p];                 //  Array a ist nicht genug für Eingabelänge, erzeugen wir doppel Größe Array a*
+                }
+                a = aa;
+                a[i] = target;
+                i++;
             }
         }
-        sc.close();
 
-        int[] result = new int[ls.size()];
-        for(int i=0;i<result.length;i++){
-            result[i] = ls.get(i);
+        int[] result = new int[i];
+        for(int n=0;n<result.length;n++){           //  schneiden wir genau bis Eingabelänge ab, um keine NULLs dran enthalten zu können.
+            result[n]=a[n];
         }
+
+        sc.close();
         return result;
 
     }
 
     public int subsetSize;
+
     public AufgabeB3A1(int subsetSize) {
+        this.subsetSize = subsetSize;
+    }
+
+    public int[][] createSubsets(int[] data) {
+        if(data == null || data.length == 0) {  // Fehlerbehandlung : Wenn Eingabemenge nicht eingegeben wird.
+            return null;
+        }
+
+        /* Wir wissen schon die Länge der Eingabe (n) und Größe der Teilmenge (k). Daher können wir rechnen, wie groß die Ergebnis-Array sein muss */
+        int amountSetUp = calFac(data.length);
+        int amountSetDown = (calFac(data.length-subsetSize)*calFac(subsetSize)); // Anzahl der Teilmenge
+        int amountSet;
+        if (amountSetUp != 0 &&  amountSetDown != 0){   // Fehlerbehandlung : divide by zero vermeiden
+            amountSet = amountSetUp / amountSetDown;
+        } else {
+            return null;
+        }
+
+        int[][] result = new int[amountSet][subsetSize]; // durch Binomialkoeffizientrechnung können wir vorher festlegen, wie groß es ist.
+        
+        int[] ptr = new int [subsetSize];
+
+        for (int i=0;i<ptr.length;i++){ // Setze Pointers [k-1,k-2 ... 2,1,0]
+            ptr[i] = subsetSize-i-1;
+        }
+
+        int n = 0;
+        if(result.length != 0) {
+            for (int idx = 0; idx < ptr.length ; idx++) { // Speichere in result[0].
+                result[n][idx] = data[ptr[idx]];
+            }
+            n++;
+        } else {
+            return null;
+        }
+
+        while(n < result.length){
+            
+            if (ptr[0] == data.length-1){
+                ptr = ptrReset(ptr, data.length);       // siehe unten (Hilfsfunktion : ptrReset)
+            } else {
+                ptr[0]++;
+            }
+
+            for (int idx = 0; idx < ptr.length ; idx++) { // Speichere in result[n].
+                result[n][idx] = data[ptr[idx]];
+            }
+            
+            n++;
+        }
+        return result;
 
     }
-    public int[][] createSubsets(int[] data) {
-        return null;
-    }
+    
     public int[] removeDuplicates(int[] data) {
-        return null;
+        boolean[] marker = new boolean[data.length]; // Position der Doppelte Zahl zu markieren. true := diese Position ist schon gelegt.
+        int counter = data.length;                   // um zu zählen, wie viele Zahlen werden hingelegt. wenn irgendeine Position true setzt, verringert counter um 1.
+        for(int i=0;i<data.length;i++){
+            if(!marker[i]) {                         // Falls jetztige Position nicht true ist, beduetet data[i] ist noch nicht belegt. 
+                int target = data[i];
+                for(int j=i+1;j<data.length;j++){    // Suche die gleiche Zahl mit data[i](target). wenn wir finden, markieren wir true.
+                    if(data[j]==target){
+                        marker[j]=true;              // Gefunden, setzt dort true und counter --
+                        counter--;
+                    }
+                }
+            }
+        }
+        int[] result = new int[counter];            // insgesamt haben wir 'counter' Zahlen, die zurückgegeben werden müssen.
+        int it1 = 0;      // Iterator für result[]
+        int it2 = 0;      // Iterator für data[] und marker[].
+        while(it1<counter){
+            if(marker[it2] == false){
+                result[it1] = data[it2];
+                it1++;
+                it2++;
+            } else {    // Falls marker[it2] == true ist, fügen wir nicht hin und einfach it2 um 1 erhöhen.
+                it2++;
+            }
+        }
+
+        return result;
     }
+
+    /* Hilfsfunktion. gibt n! zurück */
+    public int calFac(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        int fac = n;
+        for(int i = n-1 ; i >= 1 ; i--) {
+            fac *= i;
+        }
+        return fac;
+
+    }
+
+    /* Hilfsfunktion. Pointers werden in richtigen Form zurücksetzen.
+    
+    wenn das 0te Pointer großer als die Länge der Data wird, setzet die Pointers in richtige Form. 
+    
+    Bsp1) Mit data.length = 5 , [5,2,1] -> [4,3,1] 
+    Bsp2) Mit data.length = 5 , [5,4,1] -> [4,3,2] 
+    */
+
+    private int[] ptrReset(int[] ptr, int d) {
+        int size = d-1;
+
+        for (int i = 0 ; i < subsetSize ; i ++) {
+            if (ptr[i] == size) {
+                size --;
+            } else {
+                ptr[i]++;
+                for (int p = i ; p >= 0 ; p--) {
+                    ptr[p] = ptr[i]+(i-p);
+                }
+                break;
+            }
+
+            if(size == -1) {
+                return null;
+            }
+        }
+        return ptr;
+    }
+    
  }
---
+++ Blatt03/AufgabeB3A2.java
@@ -0,0 +1,208 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AufgabeB3A2 {
+
+   public static void main(String[] args) {
+      int[] result;
+      try{
+         result = readStandardIn();
+      } catch (NumberFormatException e){  // Fehlerbehandlung : wenn ungültige Eingabe gegeben wird. (z.B. "a" , " " ...)
+         System.err.println("ERROR: Input is not valid."); 
+         return;
+      }
+
+      qSort(result);
+
+      if(hasDuplicate(result)){  // Fehlerbehandlung : Duplikation.
+         System.err.println(("Error: Duplicate entries in input"));
+         return;
+      }
+      
+      if (args.length == 0) { // Fehlerbehandlung : wenn k nicht eingegeben wird.
+         System.err.println("ERROR: The argument must be at least one natural number.");
+         return;
+      }
+
+      int[] ki = new int[args.length];
+      try {
+         for (int i = 0 ; i < args.length ; i++) {
+            ki[i] = Integer.parseInt(args[i]);
+            if (ki[i] < 0) {
+               System.err.println(("Error: The argument must be natural number."));
+               return;
+            }
+
+            if (ki[i] > calFac(result.length)) {
+               System.err.println(("Error: The argument is too large"));
+               return;
+            }
+         }
+      } catch (NumberFormatException e) { // Fehlerbehandlung : wenn k nicht eine Natürliche Zahl ist. ("1.1" , "1 2 a")
+         System.err.println("ERROR: The arguments must be natural number.");
+         return;
+      }
+
+      System.out.println("Sorted input :");
+      System.out.println(Arrays.toString(result));
+
+      AufgabeB3A2 a = new AufgabeB3A2(result);
+      for(int ks : ki) {
+         System.out.println("The " + ks + "-smallest permutation is:");
+         int[] erg = a.choosePermutation(ks);
+         if(erg != null) {
+            System.out.println(Arrays.toString(erg));
+         }
+
+      }
+
+   }
+
+   public static int[] readStandardIn() throws NumberFormatException {
+
+      /* Prinzipiel ist aus Übung. (dynamische Eingabespeicherung mit statische Datenstructur.) */
+
+      Scanner sc = new Scanner(System.in);
+      
+      int[] a = new int[1];
+      int i = 0;
+      while(sc.hasNextLine()) {
+         
+         double doubleValue = Double.parseDouble(sc.nextLine());
+         int target = (int) doubleValue;  // Fehlerbehandlung unnötig. Es wird in Main() abgefangen.
+         if (i < a.length) {
+            a[i] = target;
+            i++;
+         } else {
+            int[] aa = new int[a.length*2];
+            for (int p=0;p<a.length;p++){
+                  aa[p]=a[p];                 //  Array a ist nicht genug für Eingabelänge, erzeugen wir doppel Größe Array a*
+            }
+            a = aa;
+            a[i] = target;
+            i++;
+         }
+      }
+
+      int[] result = new int[i];
+      for(int n=0;n<result.length;n++){           //  schneiden wir genau bis Eingabelänge ab, um keine NULLs dran enthalten zu können.
+         result[n]=a[n];
+      }
+
+      sc.close();
+      return result;
+
+   }
+
+   /* Hilfsfunktion Qsort. aus Blatt02 */
+
+   public static int partition(int[] data , int l , int r) {
+
+      int pivot = data[l];
+      int pivot_pos = l;
+      int iterator = l + 1;
+      int right_area = r;
+
+      while(iterator <= right_area) {
+         if (data[iterator] < pivot) {
+            int save = data[pivot_pos];
+            data[pivot_pos] = data[iterator];
+            data[iterator] = save;
+            pivot_pos++;
+            iterator++;
+
+         } else if (data[iterator] > pivot) {
+            int save = data[iterator];
+            data[iterator] = data[right_area];
+            data[right_area] = save;
+            right_area--;
+
+         } else {
+            iterator++;
+         }
+      }
+      return pivot_pos;
+   }
+
+   public static void qSort(int[] data , int l , int r) {
+
+      if (l < r) {
+         int a = partition(data , l , r);
+      
+         qSort(data , l , a);
+         qSort(data , a+1 , r);
+      }
+
+   }
+
+   public static void qSort(int[] data) {
+
+      qSort(data , 0 , data.length -1);
+      
+   }
+   /* Hilfsfunktion Injektion für Duplikation.*/
+   public static boolean hasDuplicate(int[] data) {
+      for (int i = 1 ; i < data.length ; i++) {
+          if (data[i-1] == data[i]) {
+              return true;
+          }
+      }   
+      return false;
+  }
+
+  /* Hilfsfunktion. gibt n! zurück */
+  public static int calFac(int n) {
+   if(n == 0) {
+       return 1;
+   }
+   int fac = n;
+   for(int i = n-1 ; i >= 1 ; i--) {
+       fac *= i;
+   }
+   return fac;
+
+}
+
+
+   public int[] data;
+   public AufgabeB3A2(int[] data) {
+      this.data = data;
+   }
+   public int[] choosePermutation(int kSmallest) {
+      if (kSmallest <= 0) {
+         return null;
+      }
+      int kSmall = kSmallest-1; // Berücksichrigen für Index. (17te kleintes Zahl steht 16te Index.)
+      int[] temp = new int[data.length];
+      int[] copy = Arrays.copyOf(data , data.length);
+
+      int first = calFac(data.length - 1);
+      
+      for (int n = 0 ; n < temp.length ; n++) {
+         temp[n] = copy[kSmall/first];
+         
+         copy = reduceArr(copy, kSmall/first);
+         kSmall = kSmall%first;
+         first = calFac(copy.length - 1);
+      }
+
+      return temp;
+   }
+
+   public int[] reduceArr(int[] data2 , int idx) {
+
+      int[] temp = new int[data2.length-1];
+      int a = 0;
+      int b = 0;
+      while(a < temp.length) {
+         if (b == idx) {
+            b++;
+         }
+         temp[a] = data2[b];
+         a++;
+         b++;
+      }
+      return temp;
+   }
+    
+}
